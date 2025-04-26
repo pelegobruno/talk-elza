fab00505bc20d328a19e17de9b4ffa753633566b
Git"dev"brunno <pelegobruno@gmail.com>
2025-04-26 19:02:17 -0300

    Atualização geral do projeto

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,67 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/talk-elza/', // ⬅️ IMPORTANTE: base tem que ser o nome do repositório
+  base: '/fala-elza/',
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'Elza Fala 📢',
         short_name: 'ElzaTalk',
-        start_url: '/',
+        description: 'Aplicativo de comunicação com a Elza', // Adicione uma descrição
+        start_url: '/fala-elza/', // Ajuste para incluir o base
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/vite.svg',
+            src: '/android-chrome-192x192.png',
             sizes: '192x192',
-            type: 'image/svg+xml',
+            type: 'image/png',
           },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'apple touch icon',
+          },
+          {
+            src: '/maskable-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          }
         ],
       },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true // Habilita o PWA durante o desenvolvimento para testes
+      }
     }),
   ],
 });
