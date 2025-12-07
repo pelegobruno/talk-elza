81f8e5ec8d5eef0ec691a7b1ba64cf32b5ecb029
Git"dev"brunno <pelegobruno@gmail.com>
2025-12-07 13:40:08 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,37 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/talk-elza/', // ⚡ Tem que ser EXATAMENTE o nome do seu repositório
+  base: '/talk-elza/',
+
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
+        id: '/talk-elza/',
         name: 'Elza Fala 📢',
         short_name: 'ElzaTalk',
         description: 'Aplicativo de receitas e utilidades da Elza',
-        start_url: '/talk-elza/', // ✅ Corrigido
+        start_url: '/talk-elza/',
+        scope: '/talk-elza/',
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'vite.svg', // Está no "public"
+            src: 'icons/pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/svg+xml',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
     }),
   ],
